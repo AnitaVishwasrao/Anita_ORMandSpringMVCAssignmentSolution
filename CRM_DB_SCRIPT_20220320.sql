91aba632db0b69d4f3391e5a8d8de3a610508c90
Anita Vishvasrao <vishwasrao.anita@yahoo.com>
2022-03-20 17:45:17 +0530

    added database script

---
+++ CRM_DB_SCRIPT_20220320.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS customer_relationship_management;
+
+USE customer_relationship_management;
+
+DROP TABLE IF EXISTS customer;
+
+-- please uncomment below if want to create customer table from workbench
+-- CREATE TABLE customer (
+--     customer_id INT AUTO_INCREMENT,
+--     first_name VARCHAR(50) NOT NULL,
+--     last_name VARCHAR(50) NOT NULL,
+--     email VARCHAR(30) NOT NULL,
+--     PRIMARY KEY (customer_id)
+-- );
+
